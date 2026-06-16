88b2834a9c808d7c21c0247e06d597de9e63e5f8
Ryan Coutinho Barra <ryancoutbarra1401@gmail.com>
2026-06-16 04:35:18 -0300

    Adicionando projeto completo

---
+++ app.py
@@ -0,0 +1,30 @@
+#Requisição HTTP
+import requests
+import json
+
+url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json' 
+
+response = requests.get(url) #Nossa URL vira um endpoint, responsável por mostrar os dados apenas
+print(response)
+
+if response.status_code == 200:#status_code = 200 indica que a requisição foi um sucesso
+    dados_json = response.json()
+    dados_restaurante = {}
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+        
+        dados_restaurante[nome_do_restaurante].append({
+            "item": item['Item'], 
+            "price": item['price'],
+            "description": item['description']
+    })
+    
+else:
+    print(f"O erro de requisição foi {response.status_code}")
+
+for nome_do_restaurante,dados in dados_restaurante.items():
+    nome_do_arquivo = f'{nome_do_restaurante}.json'
+    with open(nome_do_arquivo,'w') as arquivo_restaurantes:
+        json.dump(dados,arquivo_restaurantes,indent=4)
